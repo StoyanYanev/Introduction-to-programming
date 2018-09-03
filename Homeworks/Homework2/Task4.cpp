4f4bfb470add80a1e9650ef28af88a7c7da6a912
StoyanYanev <stoyanyanev98@gmail.com>
2018-09-03 15:53:34 +0300

    Update and rename fn62240_d2_4_vc.cpp to Task4.cpp

--- Homeworks/Homework2/Task4.cpp
+++ Homeworks/Homework2/Task4.cpp
@@ -1,17 +1,3 @@
-/**
-*
-* Solution to homework task
-* Introduction to programming course
-* Faculty of Mathematics and Informatics of Sofia University
-* Winter semester 2017/2018
-*
-* @author Stoyan Yanev
-* @idnumber 62240
-* @task 4
-* @compiler VC
-*
-*/
-
 #include <iostream>
 #include <vector>
 
